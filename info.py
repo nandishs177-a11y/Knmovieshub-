962fbf25a4fb750e7f15821233648a1d5ba82042
nandishs177-a11y <nandishs177@gmail.com>
2026-03-08 02:16:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,15 +18,15 @@ API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1296482719').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/Nandi177") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1003775244180'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/dailyhubdeal')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1003337417077').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://nandishs177_db_user:Nandishs17@ac-rm8h8wp-shard-00-00.mqvhjgv.mongodb.net:27017,ac-rm8h8wp-shard-00-01.mqvhjgv.mongodb.net:27017,ac-rm8h8wp-shard-00-02.mqvhjgv.mongodb.net:27017/?ssl=true&replicaSet=atlas-kmzupv-shard-0&authSource=admin&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "autofilter")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
